dfd70c940315a13c38c38c65e481bb14a98f217e
Alexandre Brilhante <alexandre.brilhante@gmail.com>
2024-06-29 17:55:18 -0400

    renamed avro schema

--- scripts/run_postgres.sh
+++ scripts/run_postgres.sh
@@ -2,7 +2,7 @@
 
 pulsar-daemon start standalone
 
-pulsar-admin schemas upload test -f $PWD/pulsar/connectors/avro-schema
+pulsar-admin schemas upload test -f $PWD/pulsar/connectors/schema.avsc
 
 pulsar-admin sinks create \
     --archive $PWD/pulsar/connectors/postgres/pulsar-io-jdbc-postgres-3.2.2.nar \
